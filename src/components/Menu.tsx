901b99251cbb911b64175afe8cac875cc9c5b49e
Lukas <lucaslinoperez@gmail.com>
2025-11-20 13:10:56 +0100

    Refactor Menu component structure and styling

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -2,25 +2,23 @@ import { Link } from "react-router-dom";
 
 function Menu() {
     return ( 
-    <nav>
-        <ul>
-            <li>
-                <Link to="/Ejem1">Ejem01</Link>
-                <Link to="/Ejem2">Ejem02</Link>
-                <Link to="/Ejem3">Ejem03</Link>
-                <Link to="/Ejem4">Ejem04</Link>
-                <Link to="/Ejem5">Ejem05</Link>
-                <Link to="/Ejem6">Ejem06</Link>
-                <Link to="/Ejem7">Ejem07</Link> 
-                <Link to="/Ejem8">Ejem08</Link>
-                <Link to="/Ejem9">Ejem09</Link>
-                <Link to="/Ejem10">Ejem10</Link>
-                <Link to="/Ejem11">Ejem11</Link>
-                <Link to="/Ejercicio01">Ejercicio01</Link>
-                <Link to="/Ejercicio02">Ejercicio02</Link>
-                <Link to="/Ejercicio03">Ejercicio03</Link>
-            </li>
-        </ul>    
+    <nav className="">
+       <ul className="flex flex-row gap-4">
+            <li><Link to="/Ejem1">Ejem01</Link></li>
+            <li><Link to="/Ejem2">Ejem02</Link></li>
+            <li><Link to="/Ejem3">Ejem03</Link></li>
+            <li><Link to="/Ejem4">Ejem04</Link></li>
+            <li><Link to="/Ejem5">Ejem05</Link></li>
+            <li><Link to="/Ejem6">Ejem06</Link></li>
+            <li><Link to="/Ejem7">Ejem07</Link></li>
+            <li><Link to="/Ejem8">Ejem08</Link></li>
+            <li><Link to="/Ejem9">Ejem09</Link></li>
+            <li><Link to="/Ejem10">Ejem10</Link></li>
+            <li><Link to="/Ejem11">Ejem11</Link></li>
+            <li><Link to="/Ejercicio01">Ejercicio01</Link></li>
+            <li><Link to="/Ejercicio02">Ejercicio02</Link></li>
+            <li><Link to="/Ejercicio03">Ejercicio03</Link></li>
+        </ul> 
     </nav>
      );
 }
